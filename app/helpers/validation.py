f356b6e04d09a93218b750c3f7487017ba4e587c
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-11-29 09:46:43 +0600

    Create validation.py

---
+++ app/helpers/validation.py
@@ -0,0 +1,18 @@
+import re
+
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(pattern, email):
+        return True
+    return False
+
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    if not re.search(r'[a-z]', password):
+        return False
+    if not re.search(r'[A-Z]', password):
+        return False
+    if not re.search(r'[0-9]', password):
+        return False
+    return True

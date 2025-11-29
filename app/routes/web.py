d7b96ee4093f22b4dc6d127b6ea9dbb3198eadbf
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-11-29 09:45:31 +0600

    Update web.py

--- app/routes/web.py
+++ app/routes/web.py
@@ -1,8 +1,35 @@
 from flask import Blueprint
 from app.controllers.customer import CustomerController
+from app.controllers.product import ProductController
+from app.controllers.order import OrderController
+from app.controllers.worker import WorkerController
 
 web = Blueprint('web', __name__)
 
 @web.route('/customer/register', methods=['POST'])
 def register_customer():
     return CustomerController().register()
+
+@web.route('/product', methods=['POST'])
+def create_product():
+    return ProductController().create()
+
+@web.route('/product', methods=['GET'])
+def get_products():
+    return ProductController().get_all()
+
+@web.route('/order', methods=['POST'])
+def create_order():
+    return OrderController().create()
+
+@web.route('/order', methods=['GET'])
+def get_orders():
+    return OrderController().get_all()
+
+@web.route('/worker', methods=['POST'])
+def create_worker():
+    return WorkerController().create()
+
+@web.route('/worker', methods=['GET'])
+def get_workers():
+    return WorkerController().get_all()
